0157abdfef4e2c7afa2361b5931dcfbd6f13b046
Andrew Hutton <fromalaska49@gmail.com>
2017-01-24 16:21:51 -0600

    Uploading Development Code Base

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require('common/head-includes.php');
+		?>
+		<title>
+			Test
+		</title>
+	</head>
+	<body>
+		<?php
+			require('common/menu.php');
+		?>
+		<div id="page-container" style="height:1000px">
+			<!--<div id="stars-container">-->
+				<div id="group1" style="">
+					<div class="star-layer-3">
+						<div id="star-layer-3">
+						</div>
+					</div>
+					<div class="star-layer-2">
+						<div id="star-layer-2">
+						</div>
+					</div>
+					<div class="star-layer-1">
+						<div id="star-layer-1">
+						</div>
+					</div>
+				</div>
+			<!--</div>-->
+			<div id="content" class="container" style="overflow-y:scroll;">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="row section-container">
+							<div class="col-sm-12">
+								100 Bit Tech is a San Antonio startup. Founded by students from UTSA, and beginning by developing an app for UTSA Underground, we are developing customizable apps that event managers and marketing gurus will be able to use to update their app on the fly and collect leads. 
+							</div>
+						</div>
+						<div class="row section-container">
+							<div class="col-sm-12">
+								100 Bit Tech is a San Antonio startup. Founded by students from UTSA, and beginning by developing an app for UTSA Underground, we are developing customizable apps that event managers and marketing gurus will be able to use to update their app on the fly and collect leads. 
+							</div>
+						</div>
+						<div class="row section-container">
+							<div class="col-sm-12">
+								100 Bit Tech is a San Antonio startup. Founded by students from UTSA, and beginning by developing an app for UTSA Underground, we are developing customizable apps that event managers and marketing gurus will be able to use to update their app on the fly and collect leads. 
+							</div>
+						</div>
+						<div class="row section-container" id="home-page-cover-image">
+							<div class="col-sm-12">
+							</div>
+						</div>
+						<div class="row partial-section-container">
+							<div class="col-sm-12 col-md-4">
+								<div class="partial-section">
+									<h3>
+										<img src="images/icons/ic_event_black_48px.svg" class="partial-section-h-icon" />
+									</h3>
+									Market Events
+								</div>
+							</div>
+							<div class="col-sm-12 col-md-4">
+								<div class="partial-section">
+									<h3>
+										<img src="images/icons/ic_schedule_black_48px.svg" class="partial-section-h-icon" />
+									</h3>
+									Send Notifications
+								</div>
+							</div>
+							<div class="col-sm-12 col-md-4">
+								<div class="partial-section">
+									<h3>
+										<img src="images/icons/ic_room_black_48px.svg" class="partial-section-h-icon" />
+									</h3>
+									Collect Leads
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+			require('common/footer.php');
+		?>
+	</body>
+</html>
